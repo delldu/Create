c129f1597e741e094b7116588ac1fb6d2879bc95
Dell Du <18588220928@163.com>
2020-08-19 02:41:36 +0800

    Update.

--- template/micro_project/tensor_client.cc
+++ template/micro_project/tensor_client.cc
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <fstream>
+#include <sstream>
 #include <memory>
 #include <string>
 
@@ -12,6 +14,17 @@ using grpc::Status;
 using tensor::HelloRequest;
 using tensor::HelloReply;
 using tensor::TensorService;
+
+std::string tensor_uuid() {
+  int i;
+  std::ostringstream os;
+  // cat /proc/sys/kernel/random/uuid
+  std::ifstream file("/proc/sys/kernel/random/uuid");
+  os << file.rdbuf();
+  file.close();
+
+  return os.str();
+}
 
 class TensorServiceClient {
  public:
@@ -55,10 +68,10 @@ int main(int argc, char** argv) {
   // localhost at port 50051). We indicate that the channel isn't authenticated
   // (use of InsecureChannelCredentials()).
 
-  TensorServiceClient connect(grpc::CreateChannel("localhost:50051", grpc::InsecureChannelCredentials()));
-  std::string user("world");
-  std::string reply = connect.Hello(user);
-  std::cout << "TensorService received: " << reply << std::endl;
+  // TensorServiceClient connect(grpc::CreateChannel("localhost:50051", grpc::InsecureChannelCredentials()));
+  // std::string user("world");
+  // std::string reply = connect.Hello(user);
+  // std::cout << "TensorService received: " << reply << std::endl;
 
   // tensor::TensorSize size;
   // size.set_n(10);
@@ -67,6 +80,9 @@ int main(int argc, char** argv) {
   // size.set_w(40);
 
   // std::cout << "size: " << size.n() << "x" << size.c() << "x" << size.h() << "x" << size.w() << std::endl;
+
+  std::string uuid = tensor_uuid();
+  std::cout << "uuid: " << uuid << std::endl;
 
   return 0;
 }
