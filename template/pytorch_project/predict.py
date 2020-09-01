7f355f99cf43f72e543b539b524c5f47f3980480
Dell Du <18588220928@163.com>
2020-09-01 23:21:06 +0800

    Update

--- template/pytorch_project/predict.py
+++ template/pytorch_project/predict.py
@@ -11,11 +11,13 @@
 # ************************************************************************************/
 #
 import os
+import glob
 import argparse
 import torch
 import torchvision.transforms as transforms
 from PIL import Image
 from model import get_model, model_load, model_setenv
+from tqdm import tqdm
 
 if __name__ == "__main__":
     """Predict."""
@@ -38,13 +40,20 @@ if __name__ == "__main__":
     if os.environ["ENABLE_APEX"] == "YES":
         model, = amp.initialize(model, opt_level="O1")
 
-    image = Image.open(args.input).convert("RGB")
-    tensor = transforms.ToTensor()(image)
-    input = tensor.to(device)
+    totensor = transforms.ToTensor()
+    toimage = transforms.ToPILImage()
 
-    # Predict without calculating gradients
-    with torch.no_grad():
-        output = model(input)
+    image_filenames = glob.glob(args.input)
+    progress_bar = tqdm(total = len(image_filenames))
 
-    # xxxx--modify here
-    print(output)
+    for index, filename in enumerate(image_filenames):
+        progress_bar.update(1)
+
+        image = Image.open(filename).convert("RGB")
+        input_tensor = totensor(image).unsqueeze(0).to(device)
+
+        with torch.no_grad():
+            output_tensor = model(input_tensor).clamp(0, 1.0).squeeze()
+
+        # xxxx--modify here
+        toimage(output_tensor.cpu()).show()
