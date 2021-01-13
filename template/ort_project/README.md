656eb3be1660f86d4bf98a71bdb956951f658383
Dell Du <18588220928@163.com>
2021-01-13 19:46:09 +0800

    Support onnxruntime.

---
+++ template/ort_project/README.md
@@ -0,0 +1,6 @@
+
+
+
+
+
+https://github.com/microsoft/onnxruntime/edit/master/csharp/test/Microsoft.ML.OnnxRuntime.EndToEndTests.Capi/C_Api_Sample.cpp
---
+++ template/ort_project/main.c
@@ -0,0 +1,50 @@
+/************************************************************************************
+***
+***	Copyright 2021 Dell(18588220928g@163.com), All Rights Reserved.
+***
+***	File Author: Dell, 2021-01-12 23:52:44
+***
+************************************************************************************/
+
+
+#include <stdio.h>
+#include <unistd.h>
+#include <getopt.h>
+#include <stdlib.h>
+
+
+void help(char *cmd)
+{
+	printf("Usage: %s [option]\n", cmd);
+	printf("    -h, --help                   Display this help.\n");
+
+	exit(1);
+}
+
+int main(int argc, char **argv)
+{
+	int optc;
+	int option_index = 0;
+
+	struct option long_opts[] = {
+		{"help", 0, 0, 'h'},
+		{0, 0, 0, 0}
+
+	};
+
+	if (argc <= 1)
+		help(argv[0]);
+
+	while ((optc = getopt_long(argc, argv, "h", long_opts, &option_index)) != EOF) {
+		switch (optc) {
+		case 'h':				// help
+		default:
+			help(argv[0]);
+			break;
+		}
+	}
+
+	printf("Hello world !\n");
+
+	return 0;
+}
